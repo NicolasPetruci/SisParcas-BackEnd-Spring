cf2e57ebcbd719092649b2eca63e0b6d1e74fbeb
MolitorLucas <lucasmolitor10@gmail.com>
2024-11-05 19:26:17 -0300

    Método para buscar usuário logado

--- src/main/java/com/sisparcas/security/service/AuthService.java
+++ src/main/java/com/sisparcas/security/service/AuthService.java
@@ -4,6 +4,8 @@ import com.sisparcas.exception.custom.NotFoundException;
 import com.sisparcas.infra.model.Usuario;
 import com.sisparcas.infra.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -24,5 +26,15 @@ public class AuthService implements UserDetailsService {
             return user.get();
         }
         throw new NotFoundException(username);
+    }
+
+    public Usuario getLoggedUser() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null) {
+            Usuario usuario = (Usuario) authentication.getPrincipal();
+            return usuario;
+        }
+        throw new IllegalArgumentException("Usuario não está logado");
     }
 }
